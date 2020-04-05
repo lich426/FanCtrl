ef2db5eb0279074e4e9fc8e5a09079fd1419f533
lich426 <lich426@gmail.com>
2020-04-05 18:17:54 +0900

    v1.0.0

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Principal;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +16,39 @@ namespace FanControl
         [STAThread]
         static void Main()
         {
+            if (IsAdministrator() == false)
+            {
+                try
+                {
+                    ProcessStartInfo procInfo = new ProcessStartInfo();
+                    procInfo.UseShellExecute = true;
+                    procInfo.FileName = Application.ExecutablePath;
+                    procInfo.WorkingDirectory = Environment.CurrentDirectory;
+                    procInfo.Verb = "runas";
+                    Process.Start(procInfo);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        public static bool IsAdministrator()
+        {
+            WindowsIdentity identity = WindowsIdentity.GetCurrent();
+            if (null != identity)
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            return false;
         }
     }
 }
